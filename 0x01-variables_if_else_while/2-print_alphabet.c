59cdbe4b8e832eaeb598e4495f4ab957a1b1a0fc
Samueltems <olawoyinsamuel3@gmail.com>
2022-09-09 18:54:17 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ * Description: using the main function
+ * program that prints the alphabeth in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+char al;
+for (al = 'a' ; al <= 'z' ; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
