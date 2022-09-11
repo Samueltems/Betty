74aadda7b9d10b682c485d6cd3e31d9e21aa54eb
Samueltems <olawoyinsamuel3@gmail.com>
2022-09-11 15:47:04 -0700

    Task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Desc: program that prints all possible different combinations of three digit
+ * Return: Always 0 (correct)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
