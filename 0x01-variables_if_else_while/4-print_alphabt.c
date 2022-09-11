c6f6831db5ab7b0a65378218ef7d58c7b9b80480
Samueltems <olawoyinsamuel3@gmail.com>
2022-09-10 17:42:30 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char 1;
+
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
