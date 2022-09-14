be7a873891bd01aebc9231116ee5afb01e569fef
Samueltems <olawoyinsamuel3@gmail.com>
2022-09-13 21:44:06 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,21 +3,19 @@
 /**
  * Main - Entry point
  * Description - a program that prints _putchar
- * Return - 0, nothing
+ * Return - 0, (success)
  */
 
-int main(void);
-int _putchar(char c)
+int main(void)
 {
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, siz;
+	siz = sizeof(str) / sizeof(int);
+	for (count = 0; count < siz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 
-		return (0);
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H_INCLUDED
+#ifndef MAIN_H
 
-#define MAIN_H_INCLUDED
+#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,7 +12,7 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add (int a, int b);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 
