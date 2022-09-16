200f11e8e9f84300f6d12729e4f46020a912e95a
Samueltems <olawoyinsamuel3@gmail.com>
2022-09-15 17:11:37 -0700

    Task 0-1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testing for printable funtions if integer is either positive or negative
+ * Return: null
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0)
+}
