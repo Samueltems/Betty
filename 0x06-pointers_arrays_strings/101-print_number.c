329c178363ab1ebc24af24e8c6f26a0a844637c5
Samueltems <olawoyinsamuel3@gmail.com>
2022-09-22 21:52:57 -0700

    Bonus Task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer.
+ * allowed to use putchar alone
+ * no arrays and pointers
+ * @n: integer we want to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+	num = n;
+	if (n < 0)
+	{
+		_putchar(45);
+
+		num = -n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
