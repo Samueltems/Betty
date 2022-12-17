2549776663150274c7b520315a903e9146b9ba06
Samueltems <olawoyinsamuel3@gmail.com>
2022-12-17 15:05:30 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,3 +6,19 @@
  */
 
 int main(void)
+{
+	int i;
+
+	printf("Infinite Loop Incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite Loop Avoided! :)\n");
+
+	return (0);
+}
