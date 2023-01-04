420f66c7334bcb1aff742d4b1fa385bb15d06da9
Samueltems <olawoyinsamuel3@gmail.com>
2023-01-03 19:05:56 -0800

    ARGH

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#define MAIN_H
+#ifndef MAIN_H
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *srs, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_cheeseboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int _putchar(char c);
+#endif/*MAIN_H*/
